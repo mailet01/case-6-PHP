91b33e1fe7e208b1c434859ea073ecc95ac59c60
mailet01 <naslundemil16@gmail.com>
2023-05-22 11:45:30 +0200

    post ig in register

--- app/public/register.php
+++ app/public/register.php
@@ -1,37 +1,49 @@
-<?php 
+<?php
 $title = "register";
 include_once "_includes/database-connection.php";
+setup_user($pdo);
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+$form_username = $POST['username'];
+    $form_hasshed_password = password_hash($POST['password'], PASSWORD_DEFAULT);
+    $sql_statement = "INSERT INTO user (`username`, `password`) VALUES ('$form_username', '$form_hashed_password')";
+    try {
+        $result = $pdo->query($sql_statement);
+        header("location: login.php");
+    } catch (PDOException $err) {
+        echo "there was a problem during the registration:" . $err->getMessage();
+    }
+}
 
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title?></title>
+    <title><?= $title ?></title>
 </head>
+
 <body>
-<h1>Register</h1>
-<?php 
-include "_includes/header.php";
+    <h1>Register</h1>
 
-?>
-<form action=""method="post">
 
-<input type="text" name="username" id="username" placeholder="username"> required
+    <?php
+    include "_includes/header.php";
 
-<input type="password" name="password" id="password" placeholder="password"> required
-<button type="submit">Register</button>
+    ?>
+    <form action="">
+        <label for="username">username</label>
+        <input type="text" name="username" id="username" placeholder="username"> required
 
-</form>
-<?php 
-$form_username = $POST['username'];
-$form_hasshed_password = password_hash($POST['password'], PASSWORD_DEFAULT);
-$sql_statement = "INSERT TO user (`username, `password`) VALUES ('$form_username', '$form_hashed_password');
-try [
-$result = $pdo->query($sql_statement);
-header("location: login.php");
-?>
+        <label for="password">password</label>
+        <input type="password" name="password" id="password" placeholder="password"> required
+
+        <button type="submit">Register</button>
+
+    </form>
+    
 </body>
+
 </html>
